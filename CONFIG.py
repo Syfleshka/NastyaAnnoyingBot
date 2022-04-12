3e5044849adc2f006acf15e8985215e2cba4838b
Alexandr Denisov <51538930+Syfleshka@users.noreply.github.com>
2022-04-12 13:17:00 +0300

    Add files via upload

---
+++ CONFIG.py
@@ -0,0 +1,8 @@
+# Config File for Telegram Bot
+#
+# Key is your test bot key
+# Target = username
+# For more info see https://github.com/Syfleshka/NastyaAnnoyingBot
+       
+KEY = 'YOURKEY111:FROMBOTFATHER1111111111111111111111'
+TARGET = "kkomilfo"
