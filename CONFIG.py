f8fa098a5abafe9ef8fcf1edaa6488a42cb145e0
Syfle <syfle.denisov@gmail.com>
2022-04-12 13:16:00 +0300

    cache

--- CONFIG.py
+++
@@ -1,8 +0,0 @@
-# Config File for Telegram Bot
-#
-# Key is your test bot key
-# Target = username
-# For more info see https://github.com/Syfleshka/NastyaAnnoyingBot
-       
-KEY = 'YOURKEY111:FROMBOTFATHER1111111111111111111111'
-TARGET = "kkomilfo"
