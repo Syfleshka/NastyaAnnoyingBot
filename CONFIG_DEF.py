aeaceaf0e828f0b314c748018934ab907a5f00a3
Syfle <syfle.denisov@gmail.com>
2022-04-12 16:28:34 +0300

    Add defaults

---
+++ CONFIG_DEF.py
@@ -0,0 +1,8 @@
+# Config File for Telegram Bot
+# For more info see https://github.com/Syfleshka/NastyaAnnoyingBot
+
+
+TARGET = 'kkomilfo'
+
+START_MESSAGE = 'Привет, я тут для того, чтобы помочь Насте учить JavaScript'
+HELP_MESSAGE = 'Я живу ради Насти, но если есть какие-то вопросы, можешь задать, обратившись ко мне'
