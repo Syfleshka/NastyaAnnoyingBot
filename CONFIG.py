7f1c68aecd23afbe86cd81a337cd8100d417a28f
Syfle <syfle.denisov@gmail.com>
2022-04-12 12:50:13 +0300

    config change

--- CONFIG.py
+++ CONFIG.py
@@ -2,6 +2,7 @@
 #
 # Key is your test bot key
 # Target = username
+# For more info see https://github.com/Syfleshka/NastyaAnnoyingBot
        
 KEY = 'YOURKEY111:FROMBOTFATHER1111111111111111111111'
 TARGET = "kkomilfo"
