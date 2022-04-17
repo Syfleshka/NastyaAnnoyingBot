9aa1b0a904771407db945bee27c99269cf543754
Syfle <syfle.denisov@gmail.com>
2022-04-17 12:01:39 +0300

    Add config

---
+++ CONFIG.py
@@ -0,0 +1,24 @@
+# Config File for Telegram Bot
+# For more info see https://github.com/Syfleshka/NastyaAnnoyingBot
+
+# TARGET = 'kkomilfo'
+# TARGET = 'Syfleshek'
+TARGET = 'kkomilfo'
+
+BOTNAME = 'kkomilfobot'
+
+
+START_MESSAGE = 'Привет, я тут для того, чтобы помочь Насте учить JavaScript'
+
+HELP_MESSAGE = 'Я живу ради Насти, но если есть какие-то вопросы, можешь задать.\n' \
+               'Просто обратись ко мне со своим вопросом. Например:\n' \
+               '@kkomilfobot, какая погода сегодня?'
+
+QUESTIONS = ['как', 'что', 'кто', 'почему']
+BORING = ["скучн"]
+JOKE = ['шутк', 'шуто']
+KIRKOROV = ['да', 'да.']
+PIDOR = ['нет', 'нет.']
+PROMISE = ['ты обещал']
+SIGN = ['!']
+COOL = [' классный?', ' классная?', ' классное?']
