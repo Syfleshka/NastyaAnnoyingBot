49d368c54f51eb63617c194181344a2c049a268f
Syfle <syfle.denisov@gmail.com>
2022-04-17 12:01:49 +0300

    refactooooooooor

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import logging
-from pathlib import Path
 import requests
 
 import telebot
@@ -7,10 +6,7 @@ from googlesearch import search
 
 from API_INFO import KEY
 
-if Path("CONFIG.py").exists():
-    from CONFIG import *
-else:
-    from CONFIG_DEF import *
+from CONFIG import *
 
 logger = telebot.logger
 telebot.logger.setLevel(logging.DEBUG)
@@ -32,61 +28,49 @@ def send_help(message):
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
+    print(message)
     if message.from_user.username == TARGET:
-        if ('Как' in message.text
-                or 'А как' in message.text
-                or 'А что' in message.text
-                or 'Что' in message.text
-                or 'Для' in message.text
-                or 'И как' in message.text
-                and '?' in message.text):
-            for searchResult in search(message.text, tld="co.in", num=1, stop=1, pause=2):
-                bot.send_message(message.chat.id,
-                                 "@" + message.from_user.username
-                                 + ", давай я тебе помогу: " + searchResult,
+
+        # Checking for question
+        if any([i in message.text.lower() for i in QUESTIONS]) and '?' in message.text.lower():
+            for search_result in search(message.text, tld="co.in", num=1, stop=1, pause=2):
+                bot.send_message(message.chat.id, f'@{message.from_user.username}, давай я помогу: {search_result}',
                                  disable_web_page_preview=True)
-        elif ("Скучн" in message.text
-              or "скучн" in message.text):
-            bot.send_message(message.chat.id,
-                             "@" + message.from_user.username
-                             + ", https://www.codewars.com/dashboard не даст тебе заскучать.",
+
+        # Checking for boring
+        elif any([i in message.text.lower() for i in BORING]):
+            bot.send_message(message.chat.id, f'@{message.from_user.username}'
+                             f', https://www.codewars.com/dashboard не даст тебе заскучать',
                              disable_web_page_preview=True)
-        elif ("Шутка" in message.text
-              or "шутка" in message.text
-              or "Шутки" in message.text
-              or "шутки" in message.text
-              or "Шуток" in message.text
-              or "шуток" in message.text):
-            bot.send_message(message.chat.id,
-                             "@" + message.from_user.username
-                             + ", шутка-хуютка.")
-        elif ("Да" == message.text
-              or "да" == message.text
-              or "Да." == message.text
-              or "да." == message.text):
-            bot.send_message(message.chat.id,
-                             "@" + message.from_user.username
-                             + ", пизда.")
-        elif ("Нет" == message.text
-              or "нет" == message.text
-              or "Нет." == message.text
-              or "нет." == message.text):
-            bot.send_message(message.chat.id,
-                             "@" + message.from_user.username
-                             + ", пидора ответ.")
-        elif ("Ты обещал" in message.text
-              or "ты обещал" in message.text):
-            bot.send_message(message.chat.id,
-                             "@" + message.from_user.username
-                             + ", к сожалению сегодня обещавший занят, попробуй в другой день.")
-    elif '@kkomilfobot' in message.text:
-        if message.text != '@kkomilfobot':
-            if (' классный?' in message.text
-                    or ' классная?' in message.text):
+
+        # Checking for stupid joke
+        elif any([i in message.text.lower() for i in JOKE]):
+            bot.send_message(message.chat.id, f'@{message.from_user.username}, шутка-хуютка.')
+
+        # Kirkorov answer
+        elif any([i == message.text.lower() for i in KIRKOROV]):
+            bot.send_message(message.chat.id, f'@{message.from_user.username}, пизда.')
+
+        # pidor answer
+        elif any([i == message.text.lower() for i in PIDOR]):
+            bot.send_message(message.chat.id, f'@{message.from_user.username}, пидора ответ.')
+
+        # promise check
+        elif any([i in message.text.lower() for i in PROMISE]):
+            bot.send_message(message.chat.id, f'@{message.from_user.username}'
+                             f', к сожалению сегодня обещавший занят, попробуй в другой день.')
+        # annoying stripped question mark check
+        elif any([i == message.text.lower() for i in SIGN]):
+            bot.send_message(message.chat.id, f'@{message.from_user.username}!')
+
+    elif f'@{BOTNAME}' in message.text:
+        if message.text != f'@{BOTNAME}':
+            if any([i in message.text.lower() for i in COOL]):
                 bot.reply_to(message, 'Да <3')
+
             else:
                 for searchResult in search(message.text, tld="co.in", num=1, stop=1, pause=2):
-                    bot.reply_to(message, "*Голосом Алисы \nВот что я нашла: " + searchResult,
+                    bot.reply_to(message, "*Голосом Алисы* \nВот что я нашла:" + searchResult,
                                  disable_web_page_preview=True)
 
 
